a58d06324787d293e0e79b0ea1c2168cf10668dc
Dera99 <masterblade213@gmail.com>
2021-06-17 14:21:14 +0700

    Initial commit

---
+++ PerpustakaanSMK/src/perpustakaansmk/UserSession.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perpustakaansmk;
+
+/**
+ *
+ * @author Atthoriq
+ */
+public class UserSession {
+    
+    private static String userLogin;
+
+    public static void setUserLogin(String userLogin) {
+        UserSession.userLogin = userLogin;
+    }
+    
+    
+    
+    
+    
+    
+    public static String getUserLogin() {
+        return userLogin;
+    }
+}
+    
